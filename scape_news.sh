1894c4c7b06929cb1e71e9d3064f303e56140f91
ShiranRog <ro.shiran@campus.technion.ac.il>
2024-08-22 13:59:57 +0300

    Update scape_news.sh

--- scape_news.sh
+++ scape_news.sh
@@ -2,14 +2,14 @@
 
 # the adress was given as a part of the exercise:
 main_adress="https://www.ynetnews.com/category/3082"
-data=$(wget -O - $main_adress 2>/dev/null)
+data=$(wget --no-check-certificate -O - $main_adress 2>/dev/null)
 # both the specific article adresses and the number of the articles are needed:
 article_urls=$(echo "$data" | grep -oP "https://www[.]ynetnews[.]com/article/[a-zA-Z0-9]+" | sort | uniq)
 num_of_lines=$(echo "$article_urls" | wc -l)
 
 for (( i=1; i<=$num_of_lines; ++i )); do
     sub_adress="$(echo "$article_urls" | head -n $i | tail -n 1)"
-    article_data=$(wget -O - $sub_adress 2>/dev/null)
+    article_data=$(wget --no-check-certificate -O - $sub_adress 2>/dev/null)
     echo -ne "$sub_adress, "
     Netanyahu_count=$(echo "$article_data" | grep -o "Netanyahu" | wc -l)
     Gantz_count=$(echo "$article_data" | grep -o "Gantz" | wc -l)
