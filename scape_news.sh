37ea010d52e1fd7cb4c1c614473314cbbbed3801
ShiranRog <ro.shiran@campus.technion.ac.il>
2024-08-22 14:44:19 +0300

    Update scape_news.sh

--- scape_news.sh
+++ scape_news.sh
@@ -9,11 +9,11 @@ num_of_lines=$(echo "$article_urls" | wc -l)
 
 for (( i=1; i<=$num_of_lines; ++i )); do
     sub_adress="$(echo "$article_urls" | head -n $i | tail -n 1)"
-    article_data=$(wget --no-check-certificate -qO - "$sub_adress")
-    Netanyahu_count=$(echo "$article_data" | grep -o "Netanyahu" | wc -l)
-    Gantz_count=$(echo "$article_data" | grep -o "Gantz" | wc -l)
+    article_data=$(wget --no-check-certificate -qO - "$sub_adress)
+    Netanyahu_count=$(echo "$article_data" | grep -oP "Netanyahu" | wc -l)
+    Gantz_count=$(echo "$article_data" | grep -oP "Gantz" | wc -l)
     # seperating between cases without and with mentions:
-    if [[ ($Netanyahu_count -eq 0) && ($Gantz_count -eq 0) ]]; then
+    if [[ $Netanyahu_count -eq 0 ]] && [[ $Gantz_count -eq 0 ]]; then
         echo "$sub_adress, -"     
     else
         echo "$sub_adress, Netanyahu, $Netanyahu_count, Gantz, $Gantz_count"
