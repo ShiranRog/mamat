1df7c30a54974c2303cb33a4490babf7b60e8167
ShiranRog <ro.shiran@campus.technion.ac.il>
2024-08-22 14:50:44 +0300

    Update scape_news.sh

--- scape_news.sh
+++ scape_news.sh
@@ -1,21 +1,23 @@
-#!/bin/bash 
+#!/bin/bash
 
-# the adress was given as a part of the exercise:
-main_adress="https://www.ynetnews.com/category/3082"
-data=$(wget --no-check-certificate -qO - "$main_adress")
-# both the specific article adresses and the number of the articles are needed:
-article_urls=$(echo "$data" | grep -oP "https://(www.)?ynetnews.com/article/[a-zA-Z0-9]*" | sort | uniq)
+# test
+main_adress="https://ynetnews.com/category/3082"
+data=$(wget -qO- "$main_adress" --no-check-certificate)
+article_urls=$(echo "$data" | grep -oP 'https://www.ynetnews.com/article/[0-9a-zA-Z]*')
+
+article_urls=$(echo "$article_urls" |sort | uniq)
 num_of_lines=$(echo "$article_urls" | wc -l)
 
-for (( i=1; i<=$num_of_lines; ++i )); do
-    sub_adress="$(echo "$article_urls" | head -n $i | tail -n 1)"
-    article_data=$(wget --no-check-certificate -qO - "$sub_adress)
-    Netanyahu_count=$(echo "$article_data" | grep -oP "Netanyahu" | wc -l)
-    Gantz_count=$(echo "$article_data" | grep -oP "Gantz" | wc -l)
-    # seperating between cases without and with mentions:
-    if [[ $Netanyahu_count -eq 0 ]] && [[ $Gantz_count -eq 0 ]]; then
-        echo "$sub_adress, -"     
-    else
-        echo "$sub_adress, Netanyahu, $Netanyahu_count, Gantz, $Gantz_count"
-    fi  
+search_N="Netanyahu"
+search_G="Gantz"
+for ((i=1; i<=num_of_lines; i++)); do
+	sub_adress=$(echo "$article_urls" | sed -n "${i}p")
+	article_data=$(wget -qO- "$sub_adress")
+	N=$(echo "$article_data" | grep -o Netanyahu | wc -l)
+	G=$(echo "$article_data" | grep -o Gantz | wc -l)
+	if [[ $N -eq 0 ]] && [[ $G -eq 0 ]]; then
+		echo "$sub_adress, -"
+	else
+		echo "$sub_adress, Netanyahu, $N, Gantz, $G"
+	fi
 done
